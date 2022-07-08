8970f91e223e0533fcca0e56b6bbccd934a16d40
Cross-bit <ondra.kryz@gmail.com>
2022-07-08 23:30:16 +0200

    executions tables

---
+++ db/003_executions.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE executions (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(64),
+    url TEXT,
+    crawlTime TEXT,
+    owner INT NOT NULL,
+    FOREIGN KEY (owner)
+        REFERENCES records(id)
+);
+
+CREATE TABLE executions_connections (
+    id SERIAL PRIMARY KEY,
+    id_from INT NOT NULL,
+    id_to INT NOT NULL,
+    FOREIGN KEY (id_from)
+        REFERENCES executions(id),
+    FOREIGN KEY (id_to)
+        REFERENCES executions(id)
+);
+
+

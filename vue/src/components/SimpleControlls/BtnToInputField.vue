1fd22e2e122984fc5f9217b644a2d4af7cdcc9b7
Cross-bit <ondra.kryz@gmail.com>
2022-07-14 01:20:09 +0200

    add new tag component

--- vue/src/components/SimpleControlls/BtnToInputField.vue
+++ vue/src/components/SimpleControlls/BtnToInputField.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="row items-center">
         <div class="col-7">
-            <q-input :autofocus="true" v-model="text" label="Enter new tag"  />
+            <q-input :rules="validationRules" :autofocus="true" v-model="text" label="Enter new tag"  />
         </div>
         <div class="col-5">
-            <q-btn v-if="text.trim()"
+            <q-btn
                 class="q-ma-sm"
                 color="primary"
                 size="md"
@@ -18,12 +18,29 @@
 
 <script setup lang="ts" >
 import { ref } from 'vue';
+import { useInsertTagMutation } from "../../graphql/_generated"
 
 const text = ref();
+const createTag = useInsertTagMutation();
+
+const validationCheck = ref();
+
+const validationRules = [
+        val => validationCheck.value = val.length <= 10 || 'Please use maximum 10 characters',
+        val => validationCheck.value = val.indexOf(' ') < 0 || 'Whitespaces not allowed'
+    ]
+
 
 const handleAddNewTag = () => {
-    text.value = "";
-    // todo:
+
+validationCheck.value === true && createTag.executeMutation({
+        tag_name: text.value
+    }
+    ).then((res) => {
+        text.value = ""; // todo:přidat něco smysluplného na succes a failure
+    }).catch(() => {
+        text.value = "";
+    });
 }
 
 </script>

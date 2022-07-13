ba5d419946f62f755f87ce14c3f9e17741657133
Cross-bit <ondra.kryz@gmail.com>
2022-07-13 15:36:11 +0200

    ajax rest api testing to records form

--- vue/src/components/EditRecordForm.vue
+++ vue/src/components/EditRecordForm.vue
@@ -6,12 +6,15 @@
     <q-input type="number" v-model="record.periodicity" label="Periodicity (seconds)" />
     <q-toggle v-model="record.active" label="Is active?" />
     <q-btn @click="updateHandler" color="primary" label="Update record" />
+    <q-btn @click="deleteHandler" color="negative" label="Delete record" />
   </q-form>
 </template>
 
 <script setup lang="ts">
 import { defineProps, ref } from 'vue';
 import { useUpdateRecordMutation, Records } from '../graphql/_generated';
+import { api } from '../boot/axios'
+//import axios from 'axios'
 
 const props = defineProps<{
   record: Records
@@ -24,5 +27,26 @@ const updateRecord = useUpdateRecordMutation()
 const updateHandler = () => {
   updateRecord.executeMutation({...record.value})
 }
+
+// const data = ref(null)
+
+const deleteHandler = async () => {
+
+/*  fetch('http://localhost:5000/api/v1/records/1', {
+            method: "get",
+            headers: { "Content-Type": "application/json" }
+ }).then((val)=> {
+  return val.json();
+  console.log(val.body)}).catch((err) =>{
+
+  console.log(err);
+ }).then((data)=> {console.log(data)})*/
+const data = ref(null)
+api.get('/records/1').then((response) => {
+        data.value = response.data
+        console.log(data.value);
+      })
+
+  }
 
 </script>

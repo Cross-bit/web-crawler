8e6e4478cffc440a9f8bf9a7356a24e949271759
Cross-bit <ondra.kryz@gmail.com>
2023-02-28 20:20:13 +0100

    small edits

--- db/init/000_readme.sql
+++ db/init/000_readme.sql
@@ -4,6 +4,8 @@ Initial db construction. Files are executed in alphabeticall order ( => based on
 NOTE!: The db is initialized only on the first run => you need to rebuild the container 
 + delete db volume in order to run these scripts...
 (another option is to delete postgres data under /var/lib/postgresql/data)
+(anoter simplest option is: docker compose rm <db-service-name> && docker compose up,
+this will do the trick too. Deleeting container from UI doesnt clean volumes)
 (another working more userfriendly option?? TODO:)
 
 */
--- db/init/004_executions.sql
+++ db/init/004_executions.sql
@@ -1,21 +1,30 @@
 CREATE TYPE executionState AS ENUM ('created', 'planned', 'waiting', 'running', 'incomplete', 'done');
+
 /*
     'planned' – in execution queue, ready to be executed ASAP
     'waiting' – waiting in system(e.g. by cron) to be planned to execution queue
-    'running' – is beeing executed
-    'incomplete' – if smth fails during execution and is beeing terminated
-    'done' – execution succesfully finished
+    'running' – is being executed
+    'incomplete' – if something fails during execution and is being terminated
+    'done' – execution successfully finished
 */
 
 CREATE TABLE executions (
     id SERIAL PRIMARY KEY,
-    creation TIME,
+    creation_time TIME,
     start_time TIME,
-    execution_time TIME,
+    duration_time INT NOT NULL,
+    is_timed boolean, 
     state_of_execution executionState,
     record_id INT NOT NULL,
     FOREIGN KEY (record_id)
         REFERENCES records(id) ON DELETE CASCADE
 );
 
-COMMENT ON TABLE executions IS 'Represents all executions for given record.';
+COMMENT ON TABLE executions IS 'Creation time of the execution.';
+
+COMMENT ON COLUMN executions.creation_time IS 'Creation time of the execution. This is a read-only field.';
+COMMENT ON COLUMN executions.start_time IS 'When did the execution started';
+COMMENT ON COLUMN executions.duration_time IS 'How much time the execution took.';
+COMMENT ON COLUMN executions.is_timed IS 'Whether the execution should create the next execution.';
+COMMENT ON COLUMN executions.state_of_execution IS 'The current state of the execution.';
+COMMENT ON COLUMN executions.record_id IS 'The ID of the record to which the execution belongs.';
